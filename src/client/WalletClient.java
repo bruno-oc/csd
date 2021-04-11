ec553643aacd9dc650b92045800ea36a65b37cf7
Luís Chula <l.chula@campus.fct.unl.pt>
2021-04-11 18:43:55 +0100

    wallet service implemented

---
+++ src/client/WalletClient.java
@@ -0,0 +1,84 @@
+package client;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+
+import api.rest.WalletService;
+import server.CoinServer;
+import server.InsecureHostnameVerifier;
+
+public class WalletClient {
+	
+	public final static int MAX_RETRIES = 3;
+	public final static long RETRY_PERIOD = 10000;
+	public final static int CONNECTION_TIMEOUT = 10000;
+	public final static int REPLY_TIMEOUT = 6000;
+
+	private static Client restClient;
+
+	public WalletClient() {
+		restClient = this.startClient();
+	}
+	
+	private Client startClient() {
+		HttpsURLConnection.setDefaultHostnameVerifier(new InsecureHostnameVerifier());
+		ClientConfig config = new ClientConfig();
+		// How much time until timeout on opening the TCP connection to the server
+		config.property(ClientProperties.CONNECT_TIMEOUT, CONNECTION_TIMEOUT);
+		// How much time to wait for the reply of the server after sending the request
+		config.property(ClientProperties.READ_TIMEOUT, REPLY_TIMEOUT);
+		Client client = ClientBuilder.newClient(config);
+
+		return client;
+	}
+	
+	public static double obtainCoin(String who, double amount) {
+		
+		WebTarget target = restClient.target(CoinServer.ServerURI).path(WalletService.PATH);
+
+		short retries = 0;
+
+		while (retries < MAX_RETRIES) {
+			try {
+
+				Response r = target.path("/obtain/" + who).request().accept(MediaType.APPLICATION_JSON)
+						.post(Entity.entity(amount, MediaType.APPLICATION_JSON));
+
+				if (r.getStatus() == Status.NO_CONTENT.getStatusCode() && r.hasEntity())
+					System.out.println("UserInBox Success, message posted with id: " + r.readEntity(Long.class));
+				else {
+					System.out.println("Error, HTTP error status: " + r.getStatus());
+				}
+				
+				double temp = r.readEntity(Double.class);
+				return temp;
+			} catch (ProcessingException pe) { // Error in communication with server
+				System.out.println("Timeout occurred.");
+				pe.printStackTrace(); // Could be removed
+				retries++;
+				try {
+					Thread.sleep(RETRY_PERIOD); // wait until attempting again.
+				} catch (InterruptedException e) {
+					// Nothing to be done here, if this happens we will just retry sooner.
+				}
+				System.out.println("Retrying to execute request.");
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		obtainCoin("henrique", 69);
+	}
+}
--- src/resources/Wallet.java
+++ src/resources/Wallet.java
@@ -4,9 +4,14 @@ import api.rest.WalletService;
 import java.util.List;
 
 public class Wallet implements WalletService {
+	
+	public Wallet() {
+		// TODO Auto-generated constructor stub
+	}
 
     @Override
     public double obtainCoins(String who, double amount) {
+    	System.out.println("=================== ENTREIIIIIIIIIIIIII ===================");
         return 0;
     }
 
@@ -29,4 +34,22 @@ public class Wallet implements WalletService {
     public List<Transaction> ledgerOfClientTransactions(String who) {
         return null;
     }
+
+	@Override
+	public double minerateMoney(String who) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void installSmartContract(String who, String smart_contract) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferMoneyWithSmartContract(String from, String to, double amount, String smart_contract_ref) {
+		// TODO Auto-generated method stub
+		
+	}
 }
