5a221bc1f9e86e78d8e3736a7f96a1cf4f7edf2d
Luís Chula <l.chula@campus.fct.unl.pt>
2021-04-14 15:14:46 +0100

    wallet service methods implemented

--- src/resources/Wallet.java
+++ src/resources/Wallet.java
@@ -3,58 +3,68 @@ package resources;
 import api.Transaction;
 import api.User;
 import api.rest.WalletService;
-import com.google.gson.Gson;
+import db.DataBase;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.HashMap;
 import java.util.List;
-
-import java.sql.*;
 import java.util.Map;
 
 public class Wallet implements WalletService {
-    private Gson gson;
-    private Map<String, User> users;
-    private List<Transaction> transactions;
-    private Map<String, List<Transaction>> clientTranscations;
-    private BufferedReader reader;
+	
+	private DataBase db;
 
-    public Wallet() throws SQLException, ClassNotFoundException, FileNotFoundException {
-        gson = new Gson();
-        reader = new BufferedReader(new FileReader("src/resources/users.json"));
-        users = gson.fromJson(reader, HashMap.class);
+    public Wallet() {
+        db = new DataBase();
     }
 
     @Override
     public double obtainCoins(String who, double amount) {
-        System.out.println("=================== ENTREIIIIIIIIIIIIII ===================");
-        double total = users.get(who).deposit(amount);
-        // TODO: write new amount to file
+        System.out.println("obtaincoin");
+        Map<String, User> users = db.getUsers();
+        User u = users.get(who);
+        double total = u.drawMoney(amount);
+        db.saveUsers(users);
+        
         return total;
     }
 
     @Override
     public double transferMoney(String from, String to, double amount) {
-        return 0;
+    	System.out.println("transferMoney");
+    	Map<String, User> users = db.getUsers();
+    	User u1 = users.get(from);
+    	User u2 = users.get(to);
+    	u1.drawMoney(amount);
+    	u2.deposit(amount);
+    	u1.addTransaction(from, to, amount);
+    	u2.addTransaction(from, to, amount);
+    	db.saveUsers(users);
+    	db.addTransaction(from, to, amount);
+    	
+        return 0; //idk what is supposed to be returned
     }
 
     @Override
     public double currentAmount(String who) {
-        return 0;
+    	System.out.println("currentAmount");
+    	Map<String, User> users = db.getUsers();
+    	User u = users.get(who);
+        return u.getAmount();
     }
 
     @Override
     public List<Transaction> ledgerOfGlobalTransactions() {
-        return null;
+    	System.out.println("ledgerOfGlobalTransactions");
+        return db.getTransactions();
     }
 
     @Override
     public List<Transaction> ledgerOfClientTransactions(String who) {
-        return null;
+    	System.out.println("ledgerOfClientTransactions");
+    	Map<String, User> users = db.getUsers();
+    	User u = users.get(who);
+        return u.getTransactions();
     }
-
+    
     @Override
     public double minerateMoney(String who) {
         // TODO Auto-generated method stub
