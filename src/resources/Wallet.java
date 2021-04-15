9019caedd62a1c91dcce8cf7d69824386fe7aae0
Luís Chula <l.chula@campus.fct.unl.pt>
2021-04-15 15:05:52 +0100

    methods fixed (using logs)

--- src/resources/Wallet.java
+++ src/resources/Wallet.java
@@ -1,12 +1,10 @@
 package resources;
 
-import api.Transaction;
-import api.User;
 import api.rest.WalletService;
 import db.DataBase;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Wallet implements WalletService {
 	
@@ -18,51 +16,78 @@ public class Wallet implements WalletService {
 
     @Override
     public double obtainCoins(String who, double amount) {
-        System.out.println("obtaincoin");
-        Map<String, User> users = db.getUsers();
-        User u = users.get(who);
-        double total = u.drawMoney(amount);
-        db.saveUsers(users);
+        System.out.println("obtainCoins");
+        String log = "obtainCoins: who = " + who + " amount = " + amount;  
+        db.addLog(log);
         
-        return total;
+        return getCurrentAmount(who);
     }
 
     @Override
     public double transferMoney(String from, String to, double amount) {
     	System.out.println("transferMoney");
-    	Map<String, User> users = db.getUsers();
-    	User u1 = users.get(from);
-    	User u2 = users.get(to);
-    	u1.drawMoney(amount);
-    	u2.deposit(amount);
-    	u1.addTransaction(from, to, amount);
-    	u2.addTransaction(from, to, amount);
-    	db.saveUsers(users);
-    	db.addTransaction(from, to, amount);
+    	String log = "transferMoney: from = " + from + " to = " + to + " amount = " + amount;  
+        db.addLog(log);
     	
         return amount;
+    }
+    
+    private double getCurrentAmount(String who) {
+    	double amount = 0;
+    	List<String> logs = db.getLogs();
+    	for(String log : logs) {
+    		System.out.println(log);
+    		if(log.contains(who) && log.contains("amount")) {
+    			String str = log.replaceAll("\\D+",".");
+				str = str.substring(1, str.length()-1);
+				double temp = Double.parseDouble(str);
+    			if(log.contains("obtainCoins")) {
+    				amount += temp;
+    			}
+    			if(log.contains("from = " + who)) {
+    				amount -= temp;
+    			}
+    			if(log.contains("to = " + who)) {
+    				amount += temp;
+    			}
+    		}
+    	}
+    	return amount;
     }
 
     @Override
     public double currentAmount(String who) {
     	System.out.println("currentAmount");
-    	Map<String, User> users = db.getUsers();
-    	User u = users.get(who);
-        return u.getAmount();
+        return getCurrentAmount(who);
+    }
+    
+    private List<String> getTransactions(){
+    	List<String> temp = new ArrayList<String>();
+    	List<String> logs = db.getLogs();
+    	for(String log : logs) {
+    		if(log.contains("transferMoney")) {
+    			temp.add(log);
+    		}
+    	}
+    	return temp;
     }
 
     @Override
-    public List<Transaction> ledgerOfGlobalTransactions() {
+    public List<String> ledgerOfGlobalTransactions() {
     	System.out.println("ledgerOfGlobalTransactions");
-        return db.getTransactions();
+        return getTransactions();
     }
 
     @Override
-    public List<Transaction> ledgerOfClientTransactions(String who) {
-    	System.out.println("ledgerOfClientTransactions");
-    	Map<String, User> users = db.getUsers();
-    	User u = users.get(who);
-        return u.getTransactions();
+    public List<String> ledgerOfClientTransactions(String who) {
+    	List<String> temp = new ArrayList<String>();
+    	List<String> transactions = getTransactions();
+    	for(String log : transactions) {
+    		if(log.contains(who)) {
+    			temp.add(log);
+    		}
+    	}
+    	return temp;
     }
     
     @Override
