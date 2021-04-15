e46ff088b492a7474e7bfb20a85f36083670223e
Luís Chula <l.chula@campus.fct.unl.pt>
2021-04-15 15:04:43 +0100

    just saving logs

--- src/db/DataBase.java
+++ src/db/DataBase.java
@@ -20,13 +20,11 @@ import resources.Wallet;
 
 public class DataBase {
 	
-    private Map<String, User> users;
-    private List<Transaction> transactions;
+    private List<String> logs;
 
 	public DataBase() {
-		users = getUsers();
-		transactions = getTransactions();
-		//clientTranscations = new LinkedHashMap<String, List<Transaction>>();
+		//logs = new ArrayList<String>();
+		logs = getLogs();
 	}
 	
 	private void writeData(String path, String data) {
@@ -49,62 +47,34 @@ public class DataBase {
 		return "";
 	}
 	
-	public void addUser(String name, String password, double amount) {
-		User user = new User(name, password, amount);
-		users.put(name, user);
-		saveUsers(users);
+	public void addLog(String log) {
+		logs.add(log);
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(logs);
+		writeData("src/resources/logs.json", jsonString);
 	}
 	
-	public Map<String, User> getUsers() {
-		String jsonString = readData("src/resources/users.json");;
-		
+	public List<String> getLogs() {
+		String jsonString = readData("src/resources/logs.json");;
 		Gson gson = new Gson();
-		Type type = new TypeToken<HashMap<String, User>>(){}.getType();
-        HashMap<String, User> clonedMap = gson.fromJson(jsonString, type);
-                
-		return clonedMap;
-	}
-	
-	public void saveUsers(Map<String, User> users) {
-		Gson gson = new Gson();
-		String jsonString = gson.toJson(users);
-		
-		writeData("src/resources/users.json", jsonString);
-	}
-	
-	public void addTransaction(String from, String to, double amount) {
-		Transaction t = new Transaction(from, to, amount);
-		transactions.add(t);
-		
-		Gson gson = new Gson();
-		String jsonString = gson.toJson(transactions);
-		
-		writeData("src/resources/transactions.json", jsonString);
-	}
-	
-	public List<Transaction> getTransactions() {
-		String jsonString = readData("src/resources/transactions.json");;
-		
-		Gson gson = new Gson();
-		Type type = new TypeToken<List<Transaction>>(){}.getType();
-		List<Transaction> list = gson.fromJson(jsonString, type);
-		        
+		Type type = new TypeToken<List<String>>(){}.getType();
+		List<String> list = gson.fromJson(jsonString, type);   
 		return list;
 	}
 	
 	public static void main(String[] args) {
 		DataBase db = new DataBase();
-		db.addUser("Luis", "pwd", 69.0);
-		db.addUser("Bruno", "pwd", 69.0);
-		//db.getUsers();
+		db.addLog("bla bla bla");
+		db.addLog("log2");
+		db.addLog("teste = sdcsf");
 		
-		//db.addTransaction("luis", "bruno", 10.0);
-		//db.getTransactions();
+		List<String> logs = db.getLogs();
+		System.out.println(logs.get(2));
 		
-		Wallet w = new Wallet();
-		w.obtainCoins("Luis", 9);
-		w.transferMoney("Luis", "Bruno", 10);
-		System.out.println(w.currentAmount("Luis"));
+		String str = "bla bla 4.56 bla bla";
+		str = str.replaceAll("\\D+",".");
+		str = str.substring(1, str.length()-1);
+		System.out.println(str);
 	}
 	
 }
