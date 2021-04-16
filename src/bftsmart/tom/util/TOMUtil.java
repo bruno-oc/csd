ab6746ac31163142f7957a81210db30dcc524eb3
Bruno Carvalho <bmd.carvalho@campus.fct.unl.pt>
2021-04-16 18:11:47 +0100

    Removed unused import

--- src/bftsmart/tom/util/TOMUtil.java
+++ src/bftsmart/tom/util/TOMUtil.java
@@ -34,7 +34,7 @@ import java.util.Random;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
+//import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
