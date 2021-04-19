fd9e8d28d76b9f9ff066f3f0243c49d700575dc3
Luís Chula <l.chula@campus.fct.unl.pt>
2021-04-19 19:11:38 +0100

    multiple servers

--- src/server/CoinServer.java
+++ src/server/CoinServer.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
 public class CoinServer {
 
     // public static final int PORT = 8080;
-    public static final String ServerURI = "https://127.0.1.1:8443/";
+    //public static final String ServerURI = "https://127.0.1.1:8443/";
     private static final Logger Log = Logger.getLogger(CoinServer.class.getName());
 
     static {
@@ -26,39 +26,13 @@ public class CoinServer {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s\n");
     }
 
-    private static SSLContext getContext() throws Exception {
-        KeyStore ks = KeyStore.getInstance("JKS");
-        KeyStore ts = KeyStore.getInstance("JKS");
-
-        try (FileInputStream fis = new FileInputStream("security/server.ks")) {
-            ks.load(fis, "password".toCharArray());
-        }
-
-        try (FileInputStream fis = new FileInputStream("security/truststore.ks")) {
-            ts.load(fis, "changeit".toCharArray());
-        }
-
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        kmf.init(ks, "password".toCharArray());
-
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        tmf.init(ts);
-
-        String protocol = "TLSv1.3";
-        SSLContext sslContext = SSLContext.getInstance(protocol);
-
-        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-
-        return sslContext;
-    }
-
     public static void main(String[] args) throws Exception {
 
         /*
         String ip = InetAddress.getLocalHost().getHostAddress();
-
-        String serverURI = String.format("https://%s:%s", ip, PORT)
-        */
+		*/
+        String serverURI = String.format("https://%s:%s/", args[2], args[3]);
+        
         HttpsURLConnection.setDefaultHostnameVerifier(new InsecureHostnameVerifier());
 
         WalletService wallet = new Wallet(args[0], Integer.parseInt(args[1]));
@@ -66,10 +40,9 @@ public class CoinServer {
         ResourceConfig config = new ResourceConfig();
         config.register(wallet);
 
-        SSLContext context = getContext();
-        JdkHttpServerFactory.createHttpServer(URI.create(ServerURI), config, SSLContext.getDefault());
+        JdkHttpServerFactory.createHttpServer(URI.create(serverURI), config, SSLContext.getDefault());
 
-        Log.info(String.format("%s Server ready @ %s\n", InetAddress.getLocalHost().getCanonicalHostName(), ServerURI));
+        Log.info(String.format("%s Server ready @ %s\n", InetAddress.getLocalHost().getCanonicalHostName(), serverURI));
     }
 
 }
