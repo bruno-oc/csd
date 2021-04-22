db8beddd240f2e948127b7026760a2a5c9c5bccb
Bruno Carvalho <bmd.carvalho@campus.fct.unl.pt>
2021-04-22 17:31:54 +0100

    Added operations text formats

--- src/api/Transaction.java
+++ src/api/Transaction.java
@@ -2,8 +2,16 @@ package api;
 
 import java.io.*;
 
-public class Transaction {
-	
+public class Transaction implements Serializable {
+
+	public static final String OBTAIN_COIN = "obtainCoins %s %s";
+	public static final String TRANSFER = "transferMoney from %s to %s %s";
+	public static final String CURRENT_AMOUNT = "currentAmount %s";
+	public static final String GET_ALL_TRANSCATIONS = "ledgerOfGlobalTransactions";
+	public static final String GET_USER_TRANSCATIONS = "ledgerOfClientTransactions %s";
+
+	private static final long serialVersionUID = 1L;
+
 	private String id;
 	private String operation;
 	private byte[] sig;
@@ -13,8 +21,8 @@ public class Transaction {
 		this.operation = operation;
 		this.sig = sig;
 	}
-	
-	public String getID() {
+
+    public String getID() {
 		return id;
 	}
 	
@@ -24,6 +32,33 @@ public class Transaction {
 	
 	public byte[] getSig() {
 		return sig;
+	}
+
+	public boolean contains(String user) {
+		return operation.contains(user);
+	}
+
+	public static byte[] serialize(Transaction obj) {
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			ObjectOutputStream os = new ObjectOutputStream(out);
+			os.writeObject(obj);
+			return out.toByteArray();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Object deserialize(byte[] data) {
+		try {
+			ByteArrayInputStream in = new ByteArrayInputStream(data);
+			ObjectInputStream is = new ObjectInputStream(in);
+			return is.readObject();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 }
