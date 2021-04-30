94a6e88cb87229c06f84c48941d58ecd3dadf4ac
Bruno Carvalho <bmd.carvalho@campus.fct.unl.pt>
2021-04-30 03:12:15 +0100

    Added hash to Transaction

--- src/api/Transaction.java
+++ src/api/Transaction.java
@@ -1,64 +1,83 @@
 package api;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Transaction implements Serializable {
 
-	public static final String OBTAIN_COIN = "obtainCoins %s %s";
-	public static final String TRANSFER = "transferMoney from %s to %s %s";
-	public static final String CURRENT_AMOUNT = "currentAmount %s";
-	public static final String GET_ALL_TRANSCATIONS = "ledgerOfGlobalTransactions";
-	public static final String GET_USER_TRANSCATIONS = "ledgerOfClientTransactions %s";
+    public static final String OBTAIN_COIN = "obtainCoins %s %s";
+    public static final String TRANSFER = "transferMoney from %s to %s %s";
+    public static final String CURRENT_AMOUNT = "currentAmount %s";
+    public static final String GET_ALL_TRANSCATIONS = "ledgerOfGlobalTransactions";
+    public static final String GET_USER_TRANSCATIONS = "ledgerOfClientTransactions %s";
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String id;
-	private String operation;
-	private byte[] sig;
-	
-	public Transaction(String id, String operation, byte[] sig) {
-		this.id = id;
-		this.operation = operation;
-		this.sig = sig;
-	}
+    private String id;
+    private String operation;
+    private byte[] sig;
+    private byte[] hash;
+
+    public Transaction(String id, String operation, byte[] sig) {
+        this.id = id;
+        this.operation = operation;
+        this.sig = sig;
+    }
+
+    public static byte[] serialize(Transaction obj) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectOutputStream os = new ObjectOutputStream(out);
+            os.writeObject(obj);
+            return out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object deserialize(byte[] data) {
+        try {
+            ByteArrayInputStream in = new ByteArrayInputStream(data);
+            ObjectInputStream is = new ObjectInputStream(in);
+            return is.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public String getID() {
-		return id;
-	}
-	
-	public String getOperation() {
-		return operation;
-	}
-	
-	public byte[] getSig() {
-		return sig;
-	}
+        return id;
+    }
 
-	public boolean contains(String user) {
-		return operation.contains(user);
-	}
+    public String getOperation() {
+        return operation;
+    }
 
-	public static byte[] serialize(Transaction obj) {
-		try {
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			ObjectOutputStream os = new ObjectOutputStream(out);
-			os.writeObject(obj);
-			return out.toByteArray();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public byte[] getSig() {
+        return sig;
+    }
 
-	public static Object deserialize(byte[] data) {
-		try {
-			ByteArrayInputStream in = new ByteArrayInputStream(data);
-			ObjectInputStream is = new ObjectInputStream(in);
-			return is.readObject();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
+    public boolean contains(String user) {
+        return operation.contains(user);
+    }
+
+    public byte[] getHash() {
+        return hash;
+    }
+
+    public void setHash(byte[] hash) {
+        this.hash = hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id='" + id + '\'' +
+                ", operation='" + operation + '\'' +
+                ", sig={" + operation + "}Kpriv" + '\'' +
+                ", hash=" + new String(hash) +
+                '}';
+    }
 }
