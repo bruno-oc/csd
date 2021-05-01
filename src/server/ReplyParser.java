2e001b8f7ec0a4e194c0c563f8656a1bd2097b26
Luís Chula <l.chula@campus.fct.unl.pt>
2021-05-01 17:18:32 +0100

    parser 100% working

--- src/server/ReplyParser.java
+++ src/server/ReplyParser.java
@@ -8,19 +8,13 @@ import java.io.ObjectInputStream;
 
 public class ReplyParser {
     private ReplicaReply reply;
-    private byte[] signedReply;
 
     public ReplyParser(byte[] fullReply) {
-        System.out.println("======ReplyParser======");
-
-        //reply = fullReply;
 
         try (ByteArrayInputStream byteIn = new ByteArrayInputStream(fullReply);
              ObjectInput objIn = new ObjectInputStream(byteIn)) {
 
             reply = (ReplicaReply) objIn.readObject();
-            /*Transaction t = (Transaction) objIn.readObject();
-            System.out.println(t.getID());*/
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,10 +24,6 @@ public class ReplyParser {
 
     public ReplicaReply getReply() {
         return reply;
-    }
-
-    public byte[] getSignedReply() {
-        return signedReply;
     }
 
 }
