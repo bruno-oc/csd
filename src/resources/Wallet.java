ca0743c48f175543136c5b13de7854ab7499f56f
Bruno Carvalho <bmd.carvalho@campus.fct.unl.pt>
2021-05-01 12:27:40 +0100

    Wallet Resource now uses asynchronous calls to the replicas

--- src/resources/Wallet.java
+++ src/resources/Wallet.java
@@ -12,59 +12,61 @@ import server.ReplyListenerImp;
 import server.SystemReply;
 import server.replica.RequestType;
 
-import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Wallet implements WalletService {
 
+    // TODO: add logs to proxy
     private final DataBase db;
-    private final ServiceProxy serviceProxy;
+    private final AsynchServiceProxy asynchSP;
 
     public Wallet(String filePath, int id) {
         db = new DataBase(filePath);
-        serviceProxy = new ServiceProxy(id);
+        asynchSP = new AsynchServiceProxy(id);
+    }
+
+    private SystemReply asyncReply(ByteArrayOutputStream byteOut, TOMMessageType type) throws Exception {
+        BlockingQueue<SystemReply> replyChain = new LinkedBlockingDeque<>();
+        ReplyListener replyListener = new ReplyListenerImp(replyChain, asynchSP);
+        asynchSP.invokeAsynchRequest(byteOut.toByteArray(), replyListener, type);
+
+        SystemReply reply = replyChain.take();
+        System.out.println(reply.getReplies().get(0).getOperation());
+        System.out.println(reply.getReplies().get(0).getValue());
+
+        return reply;
     }
 
     @Override
-    public double obtainCoins(String who, byte[] data) {
-        try {
-            System.out.println("obtainCoins");
+    public SystemReply obtainCoins(String who, byte[] data) {
+        System.out.println("obtainCoins");
 
-            Transaction t = (Transaction) Transaction.deserialize(data);
-            CryptoStuff.verifySignature(CryptoStuff.getKeyPair().getPublic(), t.getOperation().getBytes(), t.getSig());
+        Transaction t = (Transaction) Transaction.deserialize(data);
+        CryptoStuff.verifySignature(CryptoStuff.getKeyPair().getPublic(), t.getOperation().getBytes(), t.getSig());
 
-            try (ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-                 ObjectOutput objOut = new ObjectOutputStream(byteOut);) {
+        try (ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+             ObjectOutput objOut = new ObjectOutputStream(byteOut);) {
 
-                objOut.writeObject(RequestType.OBTAIN_COINS);
-                objOut.writeObject(t);
+            objOut.writeObject(RequestType.OBTAIN_COINS);
+            objOut.writeObject(t);
 
-                objOut.flush();
-                byteOut.flush();
+            objOut.flush();
+            byteOut.flush();
 
-                byte[] reply = serviceProxy.invokeOrdered(byteOut.toByteArray());
-                if (reply.length == 0)
-                    return -1;
-                try (ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
-                     ObjectInput objIn = new ObjectInputStream(byteIn)) {
-                    db.addLog(t);
-                    return (double) objIn.readObject();
-                }
-
-            } catch (IOException | ClassNotFoundException e) {
-                System.out.println("Exception: " + e.getMessage());
-            }
-        }catch (Exception e ) {
+            return asyncReply(byteOut, TOMMessageType.ORDERED_REQUEST);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return -1;
+        return null;
     }
 
     @Override
-    public double transferMoney(String from, String to, byte[] data) {
+    public SystemReply transferMoney(String from, String to, byte[] data) {
         System.out.println("transferMoney");
 
         Transaction t = (Transaction) Transaction.deserialize(data);
@@ -79,23 +81,16 @@ public class Wallet implements WalletService {
             objOut.flush();
             byteOut.flush();
 
-            byte[] reply = serviceProxy.invokeOrdered(byteOut.toByteArray());
-            if (reply.length == 0)
-                return -1;
-            try (ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
-                 ObjectInput objIn = new ObjectInputStream(byteIn)) {
-                //db.addLog("transferMoney from " + from + " to " + to + " " + amount);
-                return (double) objIn.readObject();
-            }
+            return asyncReply(byteOut, TOMMessageType.ORDERED_REQUEST);
 
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
         }
-        return -1;
+        return null;
     }
 
     @Override
-    public double currentAmount(String who) {
+    public SystemReply currentAmount(String who) {
         System.out.println("currentAmount");
 
         Transaction t = new Transaction(who, String.format(Transaction.CURRENT_AMOUNT, who), null);
@@ -108,36 +103,17 @@ public class Wallet implements WalletService {
 
             objOut.flush();
             byteOut.flush();
-            
-            AsynchServiceProxy asynchSP = new AsynchServiceProxy(2);
-            BlockingQueue<SystemReply> replyChain = new LinkedBlockingDeque<>();
-            ReplyListener replyListener = new ReplyListenerImp(replyChain, asynchSP);
-            asynchSP.invokeAsynchRequest(byteOut.toByteArray(), replyListener,
-                    TOMMessageType.UNORDERED_REQUEST);
-            
-            /*byte[] merda = asynchSP.invokeUnordered(byteOut.toByteArray());*/
-            //System.out.println("=>" + (String)replyChain.take().getReply());
 
-            byte[] reply = replyChain.take().getReply();
-            if (reply.length == 0)
-                return -1;
-            try (ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
-                 ObjectInput objIn = new ObjectInputStream(byteIn)) {
-                //db.addLog("currentAmount " + who);
-            	Transaction tt = (Transaction)objIn.readObject();
-            	System.out.println(tt.getID());
-                return (double) objIn.readObject();
-            }
-
+            return asyncReply(byteOut, TOMMessageType.UNORDERED_REQUEST);
         } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
         }
-        return -1;
+        return null;
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public List<Transaction> ledgerOfGlobalTransactions() {
+    public SystemReply ledgerOfGlobalTransactions() {
         System.out.println("ledgerOfGlobalTransactions");
 
         Transaction t = new Transaction(null, Transaction.GET_ALL_TRANSCATIONS, null);
@@ -151,24 +127,16 @@ public class Wallet implements WalletService {
             objOut.flush();
             byteOut.flush();
 
-            byte[] reply = serviceProxy.invokeUnordered(byteOut.toByteArray());
-            if (reply.length == 0)
-                return new LinkedList<>();
-            try (ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
-                 ObjectInput objIn = new ObjectInputStream(byteIn)) {
-                //db.addLog("ledgerOfGlobalTransactions");
-                return (List<Transaction>) objIn.readObject();
-            }
-
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Exception: " + e.getMessage());
+            return asyncReply(byteOut, TOMMessageType.UNORDERED_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return new LinkedList<>();
+        return null;
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public List<Transaction> ledgerOfClientTransactions(String who) {
+    public SystemReply ledgerOfClientTransactions(String who) {
         System.out.println("ledgerOfClientTransactions");
 
         Transaction t = new Transaction(who, String.format(Transaction.GET_USER_TRANSCATIONS, who), null);
@@ -182,19 +150,12 @@ public class Wallet implements WalletService {
             objOut.flush();
             byteOut.flush();
 
-            byte[] reply = serviceProxy.invokeUnordered(byteOut.toByteArray());
-            if (reply.length == 0)
-                return new LinkedList<>();
-            try (ByteArrayInputStream byteIn = new ByteArrayInputStream(reply);
-                 ObjectInput objIn = new ObjectInputStream(byteIn)) {
-                //db.addLog("ledgerOfClientTransactions " + who);
-                return (List<Transaction>) objIn.readObject();
-            }
 
-        } catch (IOException | ClassNotFoundException e) {
+            return asyncReply(byteOut, TOMMessageType.UNORDERED_REQUEST);
+        } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
         }
-        return new LinkedList<>();
+        return null;
     }
 
     @Override
