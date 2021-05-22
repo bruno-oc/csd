1d75689ee23e12339fbfadf3fddeb7282f6bb648
Bruno Carvalho <bmd.carvalho@campus.fct.unl.pt>
2021-05-23 00:39:02 +0100

    Added methods for mining

--- src/api/rest/WalletService.java
+++ src/api/rest/WalletService.java
@@ -1,16 +1,8 @@
 package api.rest;
-import api.Transaction;
+
 import server.SystemReply;
 
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path(WalletService.PATH)
@@ -21,50 +13,49 @@ public interface WalletService {
 
     @POST
     @Path("/obtain/{who}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-    SystemReply obtainCoins (@PathParam("who") String who, byte[] data);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply obtainCoins(@PathParam("who") String who, byte[] data);
 
     @POST
     @Path("/transfer/{from}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-    SystemReply transferMoney (@PathParam("from") String from, @QueryParam("to") String to, byte[] data);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply transferMoney(@PathParam("from") String from, @QueryParam("to") String to, byte[] data);
 
     @POST
-	@Path("/{me}")
+    @Path("/{me}")
     @Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-    SystemReply currentAmount (@PathParam("me") String me, byte[] data);
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply currentAmount(@PathParam("me") String me, byte[] data);
 
     @POST
-	@Path("/transactions")
+    @Path("/transactions")
     @Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-    SystemReply ledgerOfGlobalTransactions (@QueryParam("lastN") int lastN, byte[] data);
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply ledgerOfGlobalTransactions(@QueryParam("lastN") int lastN, byte[] data);
 
     @POST
-	@Path("/transactions/{who}")
+    @Path("/transactions/{who}")
     @Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     SystemReply ledgerOfClientTransactions(@PathParam("who") String who, @QueryParam("lastN") int lastN, byte[] data);
-    
-    @POST
-    @Path("/minerate/{who}")
-	@Produces(MediaType.APPLICATION_JSON)
-    double minerateMoney (@PathParam("who") String who);
-    
 
     @POST
-    @Path("/smart-contract/{who}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-    void installSmartContract (@PathParam("who") String who, @QueryParam("smart_contract") String smart_contract);
+    @Path("/mine/lastBlock")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply obtainLastMinedBlock(byte[] data);
 
     @POST
-    @Path("/transfer/smart-contract/{who}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-    void transferMoneyWithSmartContract (@PathParam("from") String from, @QueryParam("to") String to, @QueryParam("amount") double amount, @QueryParam("smart_contract_ref") String smart_contract_ref);
-    
+    @Path("/mine/transactions")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply pickNotMineratedTransaction(@QueryParam("n") int n, byte[] data);
+
+    @POST
+    @Path("/mine/block")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    SystemReply sendMinedBlock(byte[] data);
 }
