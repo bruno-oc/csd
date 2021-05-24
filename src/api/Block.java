6e500b7188ef5109ddf69a896187c6979fdb485a
Luís Chula <l.chula@campus.fct.unl.pt>
2021-05-24 22:37:01 +0100

    added signature, public key and id

--- src/api/Block.java
+++ src/api/Block.java
@@ -1,17 +1,27 @@
 package api;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.List;
 
 public class Block implements Serializable {
+	
+	private static final long serialVersionUID = 3L;
+	
     private final List<Transaction> transactions;
     private final byte[] hash;
     private byte[] proof;
+    
+    private String id;
+    private byte[] sig;
+    private byte[] pub;    
 
-    public Block(List<Transaction> transactions, byte[] hash, byte[] proof) {
+    public Block(List<Transaction> transactions, byte[] hash) {
         this.transactions = transactions;
         this.hash = hash;
-        this.proof = proof;
     }
 
     public List<Transaction> getTransactions() {
@@ -25,8 +35,56 @@ public class Block implements Serializable {
     public byte[] getProof() {
         return proof;
     }
+    
+    public byte[] getSig() {
+    	return sig;
+    }
+    
+    public byte[] getPub() {
+    	return pub;
+    }
+    
+    public String getId() {
+    	return id;
+    }
 
     public void setProof(byte[] proof) {
         this.proof = proof;
     }
+    
+    public void setSig(byte[] sig) {
+    	this.sig = sig;
+    }
+    
+    public void setPub(byte[] pub) {
+    	this.pub = pub;
+    }
+    
+    public void setId(String id) {
+    	this.id = id;
+    }
+    
+    public static byte[] serialize(Block obj) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectOutputStream os = new ObjectOutputStream(out);
+            os.writeObject(obj);
+            return out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object deserialize(byte[] data) {
+        try {
+            ByteArrayInputStream in = new ByteArrayInputStream(data);
+            ObjectInputStream is = new ObjectInputStream(in);
+            return is.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
 }
