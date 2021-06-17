ba7598be23f5cc9317b6f7397876a0c7f95ef495
Bruno Carvalho <bmd.carvalho@campus.fct.unl.pt>
2021-06-17 19:46:27 +0100

    Replica and Endorser running scripts launch the given number of instances

--- endorser.sh
+++ endorser.sh
@@ -1,1 +1,17 @@
-java -Djava.security.manager -Djava.security.policy==config/endorser/endorser.policy -cp target/WA-1-jar-with-dependencies.jar server.endorser.BFTEndorser $@ 
+#!/bin/bash
+
+usage() {
+	echo "usage: $0 <number-of-endorsers>"
+}
+
+# check if number of arguments is right
+if [ "$#" -lt 1 ];
+then
+	usage	
+	exit 1
+fi
+
+for ((ID=0; ID<$1; ID++))
+do
+	gnome-terminal --tab --title="Endorser $ID" -- java -Djava.security.manager -Djava.security.policy==config/endorser/endorser.policy -cp target/WA-1-jar-with-dependencies.jar server.endorser.BFTEndorser $ID 
+done
--- replica.sh
+++ replica.sh
@@ -1,1 +1,17 @@
-java -cp target/WA-1-jar-with-dependencies.jar server.replica.BFTServer $@ 
+#!/bin/bash
+
+usage() {
+	echo "usage: $0 <number-of-replicas>"
+}
+
+# check if number of arguments is right
+if [ "$#" -lt 1 ];
+then
+	usage	
+	exit 1
+fi
+
+for ((ID=0; ID<$1; ID++))
+do
+	gnome-terminal --tab --title="Replica $ID" -- java -cp target/WA-1-jar-with-dependencies.jar server.replica.BFTServer $ID 
+done
